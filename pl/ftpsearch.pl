034c53df6f0dcb239736f7998379b6f8d91edf9f
Joji Antony <simula67@hades.prometheus>
2012-03-01 20:04:34 -0500

    Retrieve file list from FTP sites

---
+++ pl/ftpsearch.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+#Prints all the files from an ftp site
+use Net::FTP;
+    
+$hostname = shift;
+die "Usage : $0 <hostname> [username] [password]\n" unless (defined($hostname));
+$user = shift;
+$user = "anonymous" unless (defined($user));
+$pass = shift;
+$pass = "ftpsearch" unless (defined($pass));
+
+sub parse_path {
+    my $conn = Net::FTP->new("$hostname", Debug => 0) or die "Cannot connect to $hostname: $@";
+    $conn->login("$user","$pass") or die "Cannot login ", $conn->message;
+    my $path = shift;
+    my $next_dir;
+    my $file;
+    $conn->cwd("$path") or die "CWD error : ", $conn->message;
+    my @directories = $conn->dir();
+    foreach $dir (@directories) {
+	if( $dir =~ m|^d.*| ) {
+	    if( ($next_dir) = ($dir =~ m|.*\s(.*)|) ) {
+		$conn->quit();
+		parse_path("$path$next_dir/");
+	    }
+	}
+	else {
+	    
+	    if( ($file) = ($dir =~ m|.*\s(.*)|) ) {
+                print "$hostname$path$file\n";
+            }
+
+	}
+    }
+
+}
+parse_path("/");
