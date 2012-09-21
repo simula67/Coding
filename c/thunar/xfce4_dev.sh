5e13df50d758f4ecbe0b7671a943019a8942857b
Joji Antony <simula67@gmail.com>
2012-09-21 20:52:34 +0530

    Moved thunar to seperate repo

--- c/thunar/xfce4_dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ $EUID -ne 0 ];
-then
-    echo "Need to be root to install stuff"
-    exit 1
-fi
-sudo apt-get install xfce4-dev-tools gtk-doc-tools libxfce4util-dev libexo-1-dev libxfce4ui-1-dev 
