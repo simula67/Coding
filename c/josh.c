3ed7906b64147fc6da784459a1364d99d3778531
Joji Antony <simula67@gmail.com>
2015-10-30 17:12:33 +0530

    Use readline to read input for josh

--- c/josh.c
+++ c/josh.c
@@ -9,42 +9,42 @@ Coded by : simula67(This is a joke, by the way)
 #include<string.h>
 #include<unistd.h>
 #include<stdlib.h>
-const int size=50;
-int main()
+#include<editline/readline.h>
+#include<sys/wait.h>
+const int size = 50;
+int main ()
 {
-  char buff[size];
-  while(1)
-    {
-      printf("Josh$");
-      fgets(buff,(size-1),stdin);
-      if(strcmp(buff,"exit\n")==0)
-	break;
-      else if(strcmp(buff,"help\n")==0)
-	{
-	  printf("\n\t\tThis is the JOji SHell\n");
-	  printf("\t\tTo execute a binary just type in its full path\n");
-	  printf("\t\tTo exit type \"exit\" and hit ENTER\n");
-	  printf("\t\tCommand line arguments are not supported just yet\n");
-	}
-      else
-	{
-	  buff[strlen(buff)-1]='\0';
-	  char *cmdln[]={buff,NULL};
-	  char *env[]={NULL};
-	  pid_t cmd=fork();
-	  if(cmd==0)
-	    {
-	      execve(buff,cmdln,env);
-	      printf("Execution failed\n");
-	      exit(1);
-	    }
-	  if(cmd<0)
-	    {
-	      printf("Fork failed\n");
-	    }
-	}
-      int status;
-      waitpid(-1,&status,0);
+  char *buff;
+  while (1) {
+    buff = readline ("Josh$");
+    if( buff == NULL ) {
+      exit(0);
     }
+    
+    if (strcmp (buff, "exit") == 0)
+      break;
+    else if (strcmp (buff, "help") == 0) {
+      printf ("\n\t\tThis is the JOji SHell\n");
+      printf ("\t\tTo execute a binary just type in its full path\n");
+      printf ("\t\tTo exit type \"exit\" and hit ENTER\n");
+      printf ("\t\tCommand line arguments are not supported just yet\n");
+    }
+    else {
+      char *cmdln[] = { buff, NULL };
+      char *env[] = { NULL };
+      pid_t cmd = fork ();
+      if (cmd == 0) {
+	execve (buff, cmdln, env);
+	printf ("Execution failed\n");
+	exit (1);
+      }
+      if (cmd < 0) {
+	printf ("Fork failed\n");
+      }
+    }
+    int status;
+    waitpid (-1, &status, 0);
+    free (buff);
+  }
   return 0;
 }
